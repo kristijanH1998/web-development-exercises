96f696b7878fdf5c860ca90965ea00b8143e6fa8
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-02-03 23:09:47 -0800

    Constructor, operator new practice problems

--- javascript.info_practice_problems/objects_basics.js
+++ javascript.info_practice_problems/objects_basics.js
@@ -100,3 +100,41 @@ let ladder = {
     }
 }
 ladder.up().down().up().showStep();
+
+//Two functions - one object
+let obj = {};
+function A() {return obj;};
+function B() {return obj;};
+let a = new A();
+let b = new B();
+console.log(a == b); //returns true
+
+//Create new Calculator
+function Calculator(){
+    this.read = function(){
+        this.a = +prompt("a?", 0);
+        this.b = +prompt("b?", 0);
+    }
+    this.sum = function(){
+        return this.a + this.b;
+    },
+    this.mul = function(){
+        return this.a * this.b;
+    }
+}
+// let calculator2 = new Calculator();
+// calculator2.read();
+// console.log("Sum=" + calculator2.sum());
+// console.log("Mul=" + calculator2.mul());
+
+//Create new Accumulator
+function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = function (){
+        this.value += +prompt("new number?", 0);
+    }
+}
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+console.log(accumulator.value);
